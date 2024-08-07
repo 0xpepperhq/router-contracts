2ed372c1002eb00bf56aa757f9ead46abba8354d
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2024-08-07 19:42:40 +0100

    router hardhat implementation

--- contracts/PepperPool.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {UniswapV3Pool} from '@uniswap/v3-core/contracts/UniswapV3Pool.sol';
-
-contract PepperPool is UniswapV3Pool {}
--- contracts/PepperPoolDeployer.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {UniswapV3PoolDeployer} from '@uniswap/v3-core/contracts/UniswapV3PoolDeployer.sol';
-
-contract PepperPoolDeployer is UniswapV3PoolDeployer {}
