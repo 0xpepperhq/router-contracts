c588202bcf9a301431bf9aeb9e9f8ae860d33445
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2024-10-31 17:35:02 +0100

    limit order deployment

--- script/DeployLimitOrder.s.sol
+++ script/DeployLimitOrder.s.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {LimitOrder} from "contracts/LimitOrder.sol";
+import {LimitOrder} from "../contracts/LimitOrder.sol";
 import {console} from "forge-std/console.sol";
 
 contract DeployLimitOrder is Script {
     // forge script script/DeployLimitOrder.s.sol:DeployLimitOrder --broadcast --account pepper-deployer
+    // forge verify-contract --chain-id 8453 0x7a1C2CC1D683455D7B65E9f405abcd3Cc60c7983 ./contracts/LimitOrder.sol:LimitOrder --constructor-args $(cast abi-encode "constructor(address,address)" 0xa90EA397380DA7f790E4062f5BF4aF470b9099AC 0xA24d75601C9b69a604A4669509CFaeeF68a1dd5B) --watch
 
     function run() external {
         vm.createSelectFork("base");
